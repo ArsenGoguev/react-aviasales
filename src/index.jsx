8e037e944cb400dc3d0c8146f16cca0d57a3b5d8
ArsenGoguev <agoguev33@gmail.com>
2024-07-31 02:19:39 +0300

    Попытка исправить ошибку в vercel

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import store from './store/store.js'
 import './index.scss'
-import App from './components/App.jsx'
+import App from './Components/App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
